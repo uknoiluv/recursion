8004b36a88d9677171c4b39cfb47c647499b2035
Adrian <uknoiluv@gmail.com>
2014-02-18 14:14:09 +0900

    20140217recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,35 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+//var getElementsByClassName = function (className) {
+//  return document.getElementsByClassName(className);
+//};
+
 
 // But in stead we're going to implement it from scratch:
+
+
 var getElementsByClassName = function (className) {
+
+  var result = [];
+  
+  var documentBody = document.body.children; 
+  
+  console.log(documentBody);
+
+  var elementLoop = function(elementList) {
+ 
+    console.log(elementList);
+
+      for(var i = 0; i < elementList.length; i++){  
+        if(elementList[i].classList.contains(className)){        
+          result.push(elementList[i]);                  
+        }
+        elementLoop(elementList[i].children);
+      }
+  }
+
+  elementLoop(documentBody);
+
+  return result;	
+    
   // your code here
 };
